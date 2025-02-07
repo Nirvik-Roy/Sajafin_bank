c07f6adc0e80f2a3f390fca8efc44f7097064bff
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-02-07 15:05:44 +0530

    up

--- src/View/SajafinBlog/BlogGridSection.jsx
+++ src/View/SajafinBlog/BlogGridSection.jsx
@@ -6,9 +6,35 @@ const BlogGridSection = () => {
       <div className="uni_container">
         <div className="blog-grid-wrapper">
           <div className="blog-grid-left">
-            
+            <div className="blog-navbar">
+              <div className="blog-navlink">Latest</div>
+              <div className="blog-navlink">Loan</div>
+              <div className="blog-navlink">Credit Card</div>
+              <div className="blog-navlink">Business</div>
+            </div>
+            <div className="blog-left-cards-wrapper">
+              <div className="blog-left-card"></div>
+              <div className="blog-left-card"></div>
+              <div className="blog-left-card"></div>
+              <div className="blog-left-card"></div>
+              <div className="blog-left-card"></div>
+            </div>
           </div>
-          <div className="blog-grid-right"></div>
+          <div className="blog-grid-right">
+            <div className="blog-search-section">
+            <i class="fa-solid fa-magnifying-glass"></i>
+              <input type="text" />
+              <button>Search</button>
+            </div>
+            <div className="follow-us">
+            <p>Follow Our Journey</p>
+            <div className="follow-us-social-media">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-twitter"></i>
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
