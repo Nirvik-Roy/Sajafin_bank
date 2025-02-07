4acf282512ce7e4084e31ff5a1ef44eac2378158
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-02-07 14:47:04 +0530

    up

--- src/View/SajafinBlog/BlogGridSection.jsx
+++ src/View/SajafinBlog/BlogGridSection.jsx
@@ -5,19 +5,10 @@ const BlogGridSection = () => {
     <>
       <div className="uni_container">
         <div className="blog-grid-wrapper">
-          <div className="blog-navbar-section">
-            <div className="blog-navlinks">
-              <div className="latest"></div>
-              <div className="loan"></div>
-              <div className="credit-card"></div>
-              <div className="business"></div>
-            </div>
-            <div className="blog-search-section">
-            <i class="fa-solid fa-magnifying-glass"></i>
-              <input type="text" placeholder='Search'/>
-              <button>Search</button>
-            </div>
+          <div className="blog-grid-left">
+            
           </div>
+          <div className="blog-grid-right"></div>
         </div>
       </div>
     </>
