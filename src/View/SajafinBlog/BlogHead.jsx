7291ffaf69aef3dc3bd9554ce92e6206755eb2ec
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-02-12 00:14:09 +0530

    up

--- src/View/SajafinBlog/BlogHead.jsx
+++ src/View/SajafinBlog/BlogHead.jsx
@@ -4,7 +4,8 @@ import BlogHeadImg from '../../assets/Group 1171275311.png'
 const BlogHead = () => {
   return (
     <>
-      <div className="blog-head-wrapper">
+    <div className="blog-head-container">
+    <div className="blog-head-wrapper">
         <div className="blog-head-left">
           <h2 className='blog-heading'>Blog</h2>
           <p className='home-to-blog-link'>Home <i class="fa-solid fa-arrow-right"></i> Blog</p>
@@ -13,6 +14,7 @@ const BlogHead = () => {
           <img src={BlogHeadImg} alt="" />
         </div>
       </div>
+    </div>
     </>
   )
 }
